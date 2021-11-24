087a19d0cf6400c90a487228eafe73d17b28011a
Aisling Paterson <aisling.paterson@conjura.com>
2021-11-24 20:39:18 +0000

    first task solution: 5ad4f10b.json

--- src/manual_solve.py
+++ src/manual_solve.py
@@ -1,23 +1,80 @@
-#!/usr/bin/python
-
 import os, sys
 import json
 import numpy as np
 import re
+import itertools
+import random
 
-### YOUR CODE HERE: write at least three functions which solve
-### specific tasks by transforming the input x and returning the
-### result. Name them according to the task ID as in the three
-### examples below. Delete the three examples. The tasks you choose
-### must be in the data/training directory, not data/evaluation.
-def solve_6a1e5592(x):
-    return x
+'''
+Aisling Paterson, student no. 21249294
+'''
 
-def solve_b2862040(x):
-    return x
+def solve_5ad4f10b(x):
+    '''  Assumptions made for this task:
+     - Background colour for this task is always black (colour code 0).
+     - Always outputs to a 3x3 grid.
+     - The shape we wish to emulate in the output will always have a width greater 
+       than one grid unit, otherwise it is 'noise'. 
+       
+       Transformation carried out:
+       1. The 'noise' is detected and wiped from the grid, with the noise colour being identified 
+       as the first row-wise element to be detected which has a different colour to neighbouring row elements, as per assumptions above.
+       2. Rows and columns consisting solely background colour (black) are removed.
+       3. The resulting array shape is mapped to the 3x3 grid. The resulting array is assumed to divide in 3 in both dimensions for this mapping, 
+       and this is performed to carry out the mapping.
+       
+    All of the training/test grids appear to solve succesfully with this function. 
+    '''
+    
+    background_colour = 0
+    x_out_shape, y_out_shape = 3,3
+    
+    rows,cols = x.shape
+    
+    # search for noise in dataset
+    for row in x:
+        for i in range(cols-1):
+            # if element is not background colour, and next element 
+            # and the element before is not the same colour - it must be noise
+            # from assumption that width of shape to be emulated is greater than one unit in width
+            if row[i] != 0 and row[i+1] != row[i] and row[i-1] != row[i]:
+                noise_colour = row[i]
+                break
+                
+    # strip noise from array - replace with background colour
+    x = np.where(x == noise_colour, background_colour, x)
+    
+    # remove rows that are all background, and then transpose to repeat for columns
+    x = x[~np.all(x == 0, axis=1)]
+    x = x.T[~np.all(x.T == 0, axis=1)]
+    #return to original orientation
+    x = x.T
+    
+    # determine pared-down shape of x
+    rows_new,cols_new = x.shape
+    # map onto 3x3 square - current grid must be divisible by 3
+    rows_new //= x_out_shape
+    cols_new //= y_out_shape
 
-def solve_05269061(x):
-    return x
+    # transform onto 3x3 grid
+    output_list = []
+    index_list = []
+    # pick indexes which we want to transform to grid
+    for i in range(0,x.shape[0],rows_new):
+        for j in range(0,x.shape[1],cols_new):
+                index_list.append((i,j))
+                
+    # create output with index list           
+    for i,j in index_list:
+        output_list.append(x[(i,j)])
+    
+    y_hat = np.array(output_list)
+    y_hat = y_hat.reshape((3,3))
+
+    # switch colour to correct colour - noise colour
+    y_hat = np.where(y_hat != background_colour, noise_colour, y_hat)
+    
+    return y_hat
 
 
 def main():
@@ -93,4 +150,3 @@ def show_result(x, y, yhat):
 
 
 if __name__ == "__main__": main()
-
